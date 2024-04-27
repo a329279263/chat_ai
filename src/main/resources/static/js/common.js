9adee8d4619f98aeda16825d8f67331e444bc955
lzh <lzh329279263@163.com>
2024-04-27 15:53:04 +0800

    feat: 区分本地和在线聊天模式

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,62 @@
+
+
+$.fn.notify = function(message, timeout) {
+    timeout = timeout || 3000; // 默认显示3秒
+
+    // 创建通知元素
+    var $notify = $('<div>').addClass('notify').text(message);
+
+    // 将通知元素添加到页面中
+    $('body').append($notify);
+
+    // 设置通知的显示位置和动画
+    $notify.css({
+        'position': 'fixed',
+        'top': '50px',
+        'left': '47%',
+        'z-index': '1000',
+        'background-color': '#333',
+        'color': '#fff',
+        'padding': '10px',
+        'border-radius': '5px',
+        'opacity': '0'
+    }).animate({ opacity: 1 }, 500, function() {
+        // 设置定时器，在指定时间后移除通知元素
+        setTimeout(function() {
+            $notify.animate({ opacity: 0 }, 500, function() {
+                $notify.remove();
+            });
+        }, timeout);
+    });
+};
+
+// 平滑滚动到底部
+function chatScrollToBottom(chat) {
+    // 如果需要，在这里添加平滑滚动逻辑
+    chat.scrollTo({top: chat.scrollHeight, behavior: "smooth"});
+}
+function copyTextToClipboard(text) {
+    // 创建一个临时的input元素
+    const tempInput = document.createElement('textarea');
+    tempInput.value = text;
+    tempInput.style.display = 'nonoe';
+    document.body.appendChild(tempInput);
+    // 选择input中的文本
+    tempInput.select();
+    document.execCommand('copy')
+    $('body').notify('复制成功');
+    // 移除临时的input元素
+    document.body.removeChild(tempInput);
+}
+// 检查code标签是否是代码块的一部分
+function isCodeBlock(codeElement) {
+    // 检查code标签是否有换行符，如果是，则认为是代码块
+    return codeElement.textContent.includes('\n');
+}
+
+
+function generateUniqueID() {
+    const timestamp = Date.now();
+    const randomString = Math.random().toString(36).substring(2, 15);
+    return `${timestamp}-${randomString}`;
+}
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,62 +1,149 @@
 
+const md = markdownit();
+const pendingMessages = {};
+let stompClient;
 
-$.fn.notify = function(message, timeout) {
-    timeout = timeout || 3000; // 默认显示3秒
 
-    // 创建通知元素
-    var $notify = $('<div>').addClass('notify').text(message);
+userInputField.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter' && !e.shiftKey) {
+        sendMessage();
+        e.preventDefault();
+    }
+});
+userInputField.addEventListener('input', function () {
+    // 动态调整行数，但不能超过最大行数
+    var currentRows = this.rows;
+    var scrollHeight = this.scrollHeight;
+    var lineHeight = this.clientHeight / currentRows; // 计算行高
+    // 根据内容滚动高度计算需要的行数
+    var newRows = Math.ceil(scrollHeight / lineHeight);
+    // 限制行数在最大行数范围内
+    newRows = Math.min(newRows, 8);
+    // 更新rows属性
+    if (newRows !== currentRows) {
+        this.rows = newRows;
+        chatScrollToBottom(chat);
+    }
+});
 
-    // 将通知元素添加到页面中
-    $('body').append($notify);
+function connect() {
+    var socket = new SockJS("/chat_ai/pc/ws");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({'Authorization': token}, function (frame) {
+        console.log('Connected: ' + frame);
 
-    // 设置通知的显示位置和动画
-    $notify.css({
-        'position': 'fixed',
-        'top': '50px',
-        'left': '47%',
-        'z-index': '1000',
-        'background-color': '#333',
-        'color': '#fff',
-        'padding': '10px',
-        'border-radius': '5px',
-        'opacity': '0'
-    }).animate({ opacity: 1 }, 500, function() {
-        // 设置定时器，在指定时间后移除通知元素
-        setTimeout(function() {
-            $notify.animate({ opacity: 0 }, 500, function() {
-                $notify.remove();
-            });
-        }, timeout);
+        stompClient.subscribe('/topic/' + groupId, function (message) {
+            let msgObj = JSON.parse(message.body);
+            console.info(msgObj.content)
+            if (msgObj.role === "user") {
+                userQuestion(msgObj);
+            } else {
+                aiAnswer(msgObj);
+            }
+        });
     });
-};
 
-// 平滑滚动到底部
-function chatScrollToBottom(chat) {
-    // 如果需要，在这里添加平滑滚动逻辑
-    chat.scrollTo({top: chat.scrollHeight, behavior: "smooth"});
-}
-function copyTextToClipboard(text) {
-    // 创建一个临时的input元素
-    const tempInput = document.createElement('textarea');
-    tempInput.value = text;
-    tempInput.style.display = 'nonoe';
-    document.body.appendChild(tempInput);
-    // 选择input中的文本
-    tempInput.select();
-    document.execCommand('copy')
-    $('body').notify('复制成功');
-    // 移除临时的input元素
-    document.body.removeChild(tempInput);
-}
-// 检查code标签是否是代码块的一部分
-function isCodeBlock(codeElement) {
-    // 检查code标签是否有换行符，如果是，则认为是代码块
-    return codeElement.textContent.includes('\n');
+    // 监听连接断开事件
+    socket.onclose = function () {
+        console.log('WebSocket连接已关闭。正在尝试在5秒内重新连接...');
+        setTimeout(connect, 5000); // 5秒后尝试重新连接
+    };
 }
 
+connect();
 
-function generateUniqueID() {
-    const timestamp = Date.now();
-    const randomString = Math.random().toString(36).substring(2, 15);
-    return `${timestamp}-${randomString}`;
+function userQuestion({ content, msgId }) {
+    let div = createMessageElement(content, 'user-message', msgId);
+    chat.appendChild(div);
+    chatScrollToBottom(chat);
+}
+
+function aiAnswer({ content, answerMsgId, initialized, completed }) {
+    let id = "ai" + answerMsgId;
+    if (initialized) {
+        let fromDiv = document.getElementById(answerMsgId);
+        if (!fromDiv) return; // 避免无效操作
+        let div = createMessageElement(content, 'bot-message', id);
+        pendingMessages[id] = content;
+        fromDiv.insertAdjacentElement("afterend", div);
+    } else {
+        pendingMessages[id] += content;
+        let div = document.getElementById(id);
+        if (!div) return
+        div.innerHTML = md.render(pendingMessages[id]);
+        if (completed) {
+            aiAnswerComplete(div, id);
+        }
+    }
+    chatScrollToBottom(chat);
+}
+
+
+
+
+
+
+// 在渲染消息后调用此函数
+function renderMessages(messages) {
+    let chatContent = document.createDocumentFragment(); // 用于批量插入DOM
+    for (const item of messages) {
+        item.initialized = true;
+        let messageElement = createMessageElement(item.content,item.role === "user" ? 'user-message' : 'bot-message',item.id);
+        chatContent.appendChild(messageElement);
+    }
+    // 批量插入所有消息元素
+    chat.appendChild(chatContent);
+    // 为每个消息的code块添加复制按钮
+    const messageDivs = document.querySelectorAll('.message');
+    messageDivs.forEach(div => createCopyBtnForCodeBlocks(div));
+    chatScrollToBottom(chat);
+}
+
+function createMessageElement(content, clazz, id) {
+    let div = document.createElement("div");
+    div.id = id;
+    div.innerHTML = md.render(content);
+    div.classList.add('message', clazz);
+    return div;
+}
+
+
+function createCopyBtnForCodeBlocks(div) {
+    // 获取所有code标签
+    const codeElements = div.getElementsByTagName('code');
+    // 遍历每个code标签并添加复制按钮
+    Array.from(codeElements).forEach(codeElement => {
+        // 检查code标签是否是代码块的一部分
+        if (isCodeBlock(codeElement)) {
+            let copyBtn = document.createElement('button');
+            copyBtn.textContent = '复制该代码';
+            copyBtn.classList.add('copy-btn');
+            copyBtn.onclick = function () {
+                // 复制code标签内的内容
+                copyTextToClipboard(codeElement.textContent);
+            };
+            // 将复制按钮添加到code标签旁边
+            codeElement.insertAdjacentElement('afterend', copyBtn);
+        }
+    });
+}
+
+
+function answerComplete() {
+    $('#send-button').show();
+    $('#stop-button').hide();
+}
+
+function answering() {
+    $('#send-button').hide();
+    $('#stop-button').show();
+}
+
+
+//停止消息需要拿到 消息id，发送到后端停止回答
+function stopAnswering() {
+    answerComplete();
+    Object.keys(pendingMessages).forEach((key) => {
+        stompClient.send(sendStopPath + key.split("ai")[1], {});
+    });
 }
